289b55955e93e9d5390d979240cbeb987a418b03
Eugene M <70715492+udjin10@users.noreply.github.com>
2024-06-08 11:05:43 +0300

    Create main.tf

---
+++ example/main.tf
@@ -0,0 +1,22 @@
+module "test-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = ["ru-central1-a","ru-central1-b"]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id,yandex_vpc_subnet.develop_b.id]
+  instance_name  = "webs"
+  instance_count = 2
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    owner= "Vasya",
+    org = "ooo_roga_and_coputa"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered 
+    serial-port-enable = 1
+  }
+
+}
